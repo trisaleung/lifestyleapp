cc622d1bc3bb68e060b83fe13b25433456523388
Trisa Leung <trisaleung1812@gmail.com>
2019-07-23 10:55:01 -0700

    First files

---
+++ main.py
@@ -0,0 +1,24 @@
+import webapp2
+from google.appengine.api import urlfetch
+import json
+import jinja2
+import os
+import random
+
+the_jinja_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions = ['jinja2.ext.autoescape'],
+    undefined = jinja2.StrictUndefined,
+    autoescape = True
+)
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+app = webapp2.WSGIApplication([
+    ("/", MainHandler),
+])
